aade8fc3f33db1dc163982421c9dbeed79e4cac3
Samuel Lira <sdasl03@github.com>
2025-11-17 16:40:00 +0000

    header added

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'tasks', component: TasksComponent },
     { path: '**', component: HomeComponent },
 ];
